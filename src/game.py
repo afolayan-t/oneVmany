ea6dbcda3270cf98154294121b867106d25e906a
Toluwase Afolayan <tafol20@bu.edu>
2020-05-05 03:54:49 -0400

    mostly completed survivor action function, added some things to dbd game class

--- src/game.py
+++ src/game.py
@@ -1,21 +1,37 @@
 import numpy as np
-
+import * from player_classes
 
 class dbd:
     """Defines state of game"""
-
+    num_games_run = 0
     def __init__(self, killer, surv_vect):
         self.gen_set = list(np.zeros((7,3))) # 0 if not fixed, 1 if fixed, and player_object if being worked on
         self.door_set = list(np.zeros((2,1))) # doors that can be opened once 5 generators are fixed
+        self.hook_set = list(np.zeros((4,2))) # 1 spot for hooked survivor, 1 spot for camping killer
         self.trap_door_open = False 
+
+
         self.num_rounds = 0
-        self.survivors = surv_vect
+        self.free_survivors = surv_vect
         self.killer = killer
+
+        self.survivors_alive = 4
+        self.gens_fixed = 0
+
+
     
     def __repr__(self):
-        pass
+        print("Game Number: " + num_games_run)
+        print("Survivors Alive: " + self.survivors_alive)
+        print("Generators Fixed: " + self.gens_fixed)
 
-    def run_round(self)
+    def fix_generator(choice):
+        """accepts generator pick from survivor"""
+
+    def open_door(choice):
+
+    def run_round(self):
+        #define gen pick order
         #run generator picks
         #check how many generators are being worked on, and have been completed
         #check if chase has been entered this round
--- src/player_classes.py
+++ src/player_classes.py
@@ -1,36 +1,88 @@
 import numpy as np
+import random
+
 
 """class player:
     def __init__():"""
 #adding items medkit(1-self heal), toolbox, 
 class survivor:
-    
-    def __init__(player_strategy):
+    num_survivors = 0
+    def __init__(player_name, player_strategy):
         """Defines Survivor strategy; selfless, selfish, trust, random, & adaptive*"""
-        self.help_p = .5 #probability to help
-        self.betray_p = .5 #probability to betray
-        self.soloist = True #defines whether agent prefers to be alone or not
+        self.survivor_num = num_survivors + 1
+        self.player_strategy = player_strategy
         self.is_injured = False #defines if you hav been injured
+        self.gens_completed = 0
         self.score = 0
+        
+        if player_strategy == "regular":
+            self.help_p = .5 #probability to help
+            self.soloist = True #defines whether agent prefers to be alone or not
+
+        num_survivors += 1
+            
 
     def __repr__(self):
-        pass 
+        print(player_name + " Strategy: " + self.player_strategy )
+        print("Score: " + self.score)
+        print(Score)
     
     def update_strategy(self, score, payoff):
         """Takes current game state and defines next rounds strategy based on this"""
+
         pass
 
     def request_help(self):
+        """requests help from a particular survivor"""
+
         pass
 
     def pick_gen(self, gen_set):
+        num_avail_gens = len(gen_set)
+        choice = random.(range(num_avail_gens))
+        return choice
+
+    def help(self, survivor):
+        """Helps survivor by unhooking or healing them"""
+
+
+    def take_action(self, game, gen_set, hook_set, door_set, is_chase = False, is_hooked = False):
+        """Chooses whether to pick a generator to work on, or go and help a teammate"""
+        help_p = self.help_p
+
+        if self.is_injured == True:
+            self.request_help()
+            return
+        if all(s==0 for s in hook_set) !=True: # not empty
+            help_decision = np.random.binomial(1,help_p)
+            if help_decision == 1: #agrees to help
+                #pick survivor to help
+                hooked_survivors = [survivor for survivor in hooks if survivor != 0]
+                surv_choice = random.choice(hooked_survivors)
+                self.help(surv_choice)
+                return
+        #pick generator
+        if game.gens_fixed < 5
+            available_gens = [gen for gen in gen_set if 0 in gen] # gives list of generators with available spot
+            game.fix(self.pick_gen)
+            return
+        else:
+            # open door
+            if all(d==0 for d in door_set):
+                door_choice = random.choice(door_set)
+                game.open_door(door_choice)
+            else:# door is already being opened, nothing else to do
+                return
             
-        pass
-
-    def work_on_gen(self)
 
 
-    def take_action(self, gen_set):
+
+
+
+                
+
+
+
 
 class killer:
 
@@ -42,3 +94,5 @@ class killer:
 
     def pick_survivor(self, survivors_discovered):
         """once a set of survivors have been discovered, picks a survivor to chase from set"""
+
+    def take_action(self, gen_set, hook_set, survivors, is_chase = False):
